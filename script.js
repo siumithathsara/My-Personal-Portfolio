d6a2204f801f02d18fdf8b2ea00bed3a09968171
Imakshi Siumi <sithuthathsara@gmail.com>
2026-03-30 15:18:53 +0530

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+const burger = document.getElementById("burger");
+const menuPanel = document.getElementById("menu-panel-id");
+const buttonClose = document.getElementById("button-close-id");
+
+burger.addEventListener("click", () => {
+    menuPanel.classList.add("active");
+    burger.classList.add("hide");
+});
+
+buttonClose.addEventListener("click", () => {
+    menuPanel.classList.remove("active");
+    burger.classList.remove("hide"); 
+});
