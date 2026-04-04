0c1a6ed261d1fd036d59702bb605787a9e040578
Imakshi Siumi <sithuthathsara@gmail.com>
2026-04-04 12:00:40 +0530

    update

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ buttonClose.addEventListener("click", () => {
     menuPanel.classList.remove("active");
     burger.classList.remove("hide");
 });
+
+
+// resume section
+
+function showResume(sectionId, element) {
+  const sections = document.querySelectorAll(".resume-content");
+  const navItems = document.querySelectorAll(".resume-nav-list li");
+
+  navItems.forEach(item => item.classList.remove("active"));
+  element.classList.add("active");
+
+  sections.forEach(sec => sec.style.display = "none");
+
+  if (sectionId === "all") {
+    sections.forEach(sec => {
+      sec.style.display = ""; 
+    });
+  } else {
+    const activeSection = document.getElementById(sectionId);
+    activeSection.style.display = ""; 
+  }
+}
+
+
+//  work section
+
